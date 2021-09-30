02304f0c93779cc9f0a51bcc392cb1c3702eb854
Roja <rojakasoju19@gmail.com>
2021-09-30 14:47:19 +0530

    all files

---
+++ src/main/java/com/gcp/pubsub/PubsubApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.pubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubApplication.class, args);
+	}
+
+}
